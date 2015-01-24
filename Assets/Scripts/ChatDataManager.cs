8ef41fdbfb5d133281b168c5ed8de2337e3581e8
jaehyun kang <jaehyun@unity3d.com>
2015-01-25 04:03:46 +0900

    roll-back
    hi

--- Assets/Scripts/ChatDataManager.cs
+++ Assets/Scripts/ChatDataManager.cs
@@ -3,100 +3,107 @@ using System.Collections;
 
 public class ChatDataManager : MonoBehaviour
 {
-    private static ChatDataManager _instance;
-    public static ChatDataManager Instance
-    {
-        get { return _instance; }
-    }
-
+	private static ChatDataManager _instance;
+	public static ChatDataManager Instance
+	{
+		get { return _instance; }
+	}
+	
 	public enum TextState
 	{
 		Processing, Finished,
 	}
-
+	
 	public TextState textState;
 	public ChatScrollView chatScrollView;
-	//public BGController heroinReaction;
+	public BGController bgController;
 	public QuestionBox questionBox;
 	public AnswerBox answerBox;
 	public AudioController bmgController;
+	public AudioController seController;
 	public UISprite characterFace;
-
-    private ChatData chatData;
+	public ChatData chatData;
 	private int curr_scene_script_index = 0;
 	private int curr_speech_index = 0;
-
+	
 	private string fileName = "SpeechDB_Test";
 	//private string fileName = "ChatDB";
-
-    void Awake()
-    {
-        if (_instance == null)
-        {
-            _instance = this;
-//			chatData = Resources.Load("Data/ChatDB") as ChatData;
+	
+	void Awake()
+	{
+		if (_instance == null)
+		{
+			_instance = this;
+			//            chatData = Resources.Load("Data/ChatDB") as ChatData;
 			chatData = Resources.Load("Data/" + fileName) as ChatData;
-
-            DontDestroyOnLoad(this.gameObject);
-        }
-        else
-        {
-            Destroy(this.gameObject);
-        }
-    }
-
+			
+			DontDestroyOnLoad(this.gameObject);
+		}
+		else
+		{
+			Destroy(this.gameObject);
+		}
+	}
+	
 	void Start()
 	{
-		//chatScrollView.UpdateScreen();
-		//change heroin face
-		//if(!GetSpeech().facelook_filename.Equals(""))
-		//	heroinReaction.ChangeFacelook(GetSpeech().facelook_filename);
+		//chatScrollView.Update_screen();
+		//change bg
+		if(GetSpeech().bg != null && !string.IsNullOrEmpty(GetSpeech().bg))
+			bgController.ChangeBG(GetSpeech().bg);
+		//Change sound
+		if(GetSpeech().bgm_filename != null && !string.IsNullOrEmpty(GetSpeech().bgm_filename))
+			bmgController.ChangeSound(GetSpeech().bgm_filename);
+		if(GetSpeech().sound_effect_filename != null && !string.IsNullOrEmpty(GetSpeech().sound_effect_filename))
+			seController.ChangeSound(GetSpeech().sound_effect_filename);
+		
 		//change box style
 		SetBoxStyle();
+		
 	}
-
+	
 	public ChatData.SceneScript.Speech GetSpeech()
 	{
 		return chatData.scene_script_list[curr_scene_script_index].speech_list[curr_speech_index];
 	}
-
+	
 	private bool isAnswered = false;
 	public void OnClick_Question()
 	{
 		if (textState == TextState.Processing)
 		{
 			chatScrollView.questionLabel.GetComponent<TypewriterEffect>().Finish();
-
+			
 			return;
 		}
-
+		
 		if ((HasToBeAnswered && isAnswered) || !HasToBeAnswered)
 		{
 			GetNextSpeech();
 		}
 	}
-
+	
 	public bool HasToBeAnswered
 	{
 		get
 		{
 			bool hasToBeAnswered = false;
-
+			
 			if (GetSpeech().speech_type == ChatData.SceneScript.Speech.TYPE.QN) hasToBeAnswered = true;
 			if (GetSpeech ().speech_type == ChatData.SceneScript.Speech.TYPE.QR) hasToBeAnswered = true;
 			if (GetSpeech().speech_type == ChatData.SceneScript.Speech.TYPE.S) hasToBeAnswered = false;
-
+			
 			return hasToBeAnswered;
 		}
 	}
-
+	
 	void GetNextSpeech()
 	{
 		if(curr_speech_index < chatData.scene_script_list[curr_scene_script_index].speech_list.Count - 1)
 		{
 			// Testing..
 			textState = TextState.Processing;
-
+			
 			curr_speech_index++;
 			chatScrollView.UpdateScreen();
 			//change heroin face
@@ -104,42 +111,58 @@ public class ChatDataManager : MonoBehaviour
 			{
 				characterFace.spriteName = GetSpeech().facelook_filename;
 			}
-			//if(!GetSpeech().facelook_filename.Equals(""))
-			//	heroinReaction.ChangeFacelook(GetSpeech().facelook_filename);
+			//change bg
+			if(GetSpeech().bg != null && !string.IsNullOrEmpty(GetSpeech().bg))
+				bgController.ChangeBG(GetSpeech().bg);
+			//Change sound
+			if(GetSpeech().bgm_filename != null && !string.IsNullOrEmpty(GetSpeech().bgm_filename))
+				bmgController.ChangeSound(GetSpeech().bgm_filename);
+			if(GetSpeech().sound_effect_filename != null && !string.IsNullOrEmpty(GetSpeech().sound_effect_filename))
+				seController.ChangeSound(GetSpeech().sound_effect_filename);
 			//change box style
 			SetBoxStyle();
-			//Change sound
-			if(GetSpeech().bgm_filename != null && !GetSpeech().bgm_filename.Equals(""))
-				bmgController.ChangeSound(GetSpeech().bgm_filename);
-
+			
 		}
 	}
-
+	
 	public void OnClick_Answer(int selectedItemIndex)
 	{
 		bool isSuccessAnswer = true;
 		//judge success or fail
-
-
+		
+		
 		//change heroin face
-		string heroin_facelook_filename = "";
+		//        string heroin_facelook_filename = "";
+		//        if(isSuccessAnswer)
+		//        {
+		//            heroin_facelook_filename = GetSpeech().answerlist[selectedItemIndex].success_facelook_filename;
+		//        }
+		//        else
+		//        {
+		//            heroin_facelook_filename = GetSpeech().answerlist[selectedItemIndex].fail_facelook_filename;
+		//        }
+		
+		//heroinReaction.ChangeFacelook(heroin_facelook_filename);
+		
+		//Change sound
+		string bgmFileName;
 		if(isSuccessAnswer)
 		{
-			heroin_facelook_filename = GetSpeech().answerlist[selectedItemIndex].success_facelook_filename;
+			bgmFileName = GetSpeech().answerlist[selectedItemIndex].success_bgm_filename;
 		}
 		else
 		{
-			heroin_facelook_filename = GetSpeech().answerlist[selectedItemIndex].fail_facelook_filename;
+			bgmFileName = GetSpeech().answerlist[selectedItemIndex].fail_bgm_filename;
 		}
-
-		//heroinReaction.ChangeFacelook(heroin_facelook_filename);
-
+		if(bgmFileName != null && !string.IsNullOrEmpty(bgmFileName))
+			bmgController.ChangeSound(bgmFileName);
+		
 		//change box style
 		ShowStoryOnly();
-
+		
 		isAnswered = true;
 	}
-
+	
 	void SetBoxStyle()
 	{
 		if(GetSpeech().speech_type == ChatData.SceneScript.Speech.TYPE.S)
@@ -156,12 +179,12 @@ public class ChatDataManager : MonoBehaviour
 		questionBox.ChangeState(QuestionBox.STATE.STORY);
 		answerBox.ChangeState(AnswerBox.STATE.STORY);
 	}
-
+	
 	void ShowChat()
 	{
 		questionBox.ChangeState(QuestionBox.STATE.QUESTION);
 		answerBox.ChangeState(AnswerBox.STATE.QUESTION);
 	}
-
-
+	
+	
 }
